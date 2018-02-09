0aa6887ed44330c792775e768087e4f51d15947b
coreykadd <coreymangan22@live.ie>
2018-02-09 16:27:55 +0000

    setup

---
+++ server.js
@@ -0,0 +1,18 @@
+//server.js
+
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var socket = require('socket.io');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+    res.send('hello');
+});
+
+app.listen(8080, function(){
+    console.log('Listening on port 8080');
+});
